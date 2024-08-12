c0123710498c6c10d78c8e12a6af08597b091ace
Mike2024New <proggerman2022@yandex.ru>
2024-08-12 21:05:08 +0300

    подключены страницы contacts и delivery

--- main/views.py
+++ main/views.py
@@ -17,3 +17,19 @@ def about(request):
         'text':'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.',
     } # тестовый контент передаваемый в шаблон
     return render(request, "main/about.html",context)
+
+def delivery(request):
+    context = {
+        'title':'Home - Доставка и оплата',
+        'content':'Доставка и оплата',
+        'text':'Доставка в этом магазине выполняется на транспорте, оплата принимается деньгами.'
+    }
+    return render(request, "main/delivery.html",context)
+
+def contacts(request):
+    context = {
+        'title':'Home - Контакты',
+        'content':'Контакты',
+        'text':'Город Москва, +777 77 77'
+    }
+    return render(request, "main/contacts.html",context)
